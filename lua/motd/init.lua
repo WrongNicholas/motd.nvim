85c4a98c8c760d2b302e3f615882fb2e9f8b109f
WrongNicholas <mnwojnar2000@gmail.com>
2025-02-09 11:48:59 -0500

    Close initial buffer

--- lua/motd/init.lua
+++ lua/motd/init.lua
@@ -34,7 +34,10 @@ function M.show_motd()
         return
     end
 
-    -- Create a new buffer
+    -- Get the initial buffer (usually buffer 1)
+    local initial_buf = vim.api.nvim_get_current_buf()
+
+    -- Create a new MOTD buffer
     local buf = vim.api.nvim_create_buf(false, true) -- No file, temporary buffer
     vim.api.nvim_buf_set_lines(buf, 0, -1, false, read_motd())
     vim.api.nvim_buf_set_option(buf, "modifiable", false) -- Read-only buffer
@@ -42,20 +45,14 @@ function M.show_motd()
     vim.api.nvim_buf_set_option(buf, "bufhidden", "wipe") -- Close automatically
     vim.api.nvim_buf_set_option(buf, "buflisted", false)
 
-
     -- Set the buffer as the current window
     vim.api.nvim_set_current_buf(buf)
-    
-    vim.api.nvim_create_autocmd("WinEnter", {
-    callback = function()
-        local current_buf = vim.api.nvim_get_current_buf()
-        if current_buf ~= buf then
-            vim.api.nvim_buf_delete(buf, { force = true })
-        end
-    end
-    })
 
+    -- If the initial buffer was `[No Name]` and it's still open, delete it
+    if vim.api.nvim_buf_is_valid(initial_buf) and vim.bo[initial_buf].buflisted then
+        vim.api.nvim_buf_delete(initial_buf, { force = true })
     end
+end
 
 return M
 
